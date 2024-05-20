fa84340e2e4a42386b75c4cd40e189ed7060bfdd
seer-lee <2seer@naver.com>
2024-05-20 23:12:28 +0900

    init: first commit

---
+++ src/main/java/com/inflearn/lecture/PracticalTestingApplication.java
@@ -0,0 +1,13 @@
+package com.inflearn.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticalTestingApplication.class, args);
+	}
+
+}
