51eccf9365b329636bf28429b000ab96f817e388
seer-lee <2seer@naver.com>
2024-05-20 23:38:07 +0900

    add : cafeKiosk 메서드 추가

--- src/main/java/com/inflearn/unit/CafeKiosk.java
+++ src/main/java/com/inflearn/unit/CafeKiosk.java
@@ -1,10 +1,32 @@
 package com.inflearn.unit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class CafeKiosk {
+    private final List<Beverage> beverages = new ArrayList<>(); // 음료 리스트
+
+    // 음료 추가
     public void add(Beverage beverage) {
+        beverages.add(beverage);
     }
 
+    // 음료 제거
+    public void remove(Beverage beverage) {
+        beverages.remove(beverage);
+    }
+
+    // 음료 리스트 초기화
+    public void clear() {
+        beverages.clear();
+    }
+
+    // 총 주문 가격 계산
     public int calculateTotalPrice() {
-        return 0;
+        int totalPrice = 0;
+        for (Beverage beverage : beverages) {
+            totalPrice += beverage.getPrice();
+        }
+        return totalPrice;
     }
 }
